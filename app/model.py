d0f0a9709c39bf9d530d845697d3d7636b070e68
nancymatijas <nancy.matijas@gmail.com>
2025-07-20 12:29:41 +0200

    Add changes

--- app/model.py
+++ app/model.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def load_model():
     try:
-        return YOLO("../runs/detect/train_yolo11s/weights/best.pt")
+        return YOLO("runs/detect/train_yolo11s/weights/best.pt")
     except Exception as e:
         st.error(f"Error in loading model: {e}")
         return None
